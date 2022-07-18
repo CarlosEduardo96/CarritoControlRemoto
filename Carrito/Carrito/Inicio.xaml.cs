7409a42b9fc0cc307d5d5229f7a59f278e5fbdd6
Carlos Eduardo <crodriguez@golmex.com.mx>
2022-07-18 00:37:01 -0500

    Vista Botones

---
+++ Carrito/Carrito/Inicio.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Carrito.View;
+namespace Carrito
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Iniciar_Clicked(object sender, EventArgs e)
+        {
+            Control c=new Control();
+            Content = c.Content;
+        }
+    }
+}
---
+++ Carrito/Carrito/Modelo/BotonModelo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Carrito.Modelo
+{
+    public enum BotonModelo
+    {
+        Arriba=1,
+        Abajo=2,
+        Izquierda=3,
+        Derecha=4,
+        Acelerar=5,
+        MarchaAtras=6,
+        Freno=7
+    }
+}
